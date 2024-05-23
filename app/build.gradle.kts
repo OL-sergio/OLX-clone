86f75a572826aca50eb1cb29b4484950b6db7d4e
sergio <a18074@alunos.ipca.pt>
2024-05-23 16:25:06 +0100

    Work Done: - BlackCat27 and Santalu have been used to create masks for price and phone.
    - Updates to the design.
    
    * Notes:
    - ...
    
    * Add items:
    - ...
    
    * Updated items:
    - activity_adverts.xml
    - activity_create_advert.xml
    - activity_my_adverts.xml
    - build.gradle
    - CreateAdvertActivity.java
    - custom_track.xml
    - dimens.xml
    - libs.versions.toml
    - settings.gradle.kts
    - strings.xml
    
    * Removed:
    - ...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -38,15 +42,21 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation(libs.constraintlayout)
-
     // Firebase
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
     implementation(libs.realtime.database)
     implementation(libs.firebase.storage)
     implementation(libs.firebase.auth)
+    //https://github.com/santalu/maskara
+    implementation(libs.maskara)
+    //https://github.com/BlacKCaT27/CurrencyEditText
+    implementation(libs.library)
+
 }
--- app/src/main/java/exemple/udemy/java/olx/activity/CreateAdvertActivity.java
+++ app/src/main/java/exemple/udemy/java/olx/activity/CreateAdvertActivity.java
@@ -1,12 +1,24 @@
 package exemple.udemy.java.olx.activity;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.blackcat.currencyedittext.CurrencyEditText;
+import com.santalu.maskara.widget.MaskEditText;
+
+import java.util.Locale;
 import java.util.Objects;
 import exemple.udemy.java.olx.R;
 import exemple.udemy.java.olx.databinding.ActivityCreateAdvertBinding;
@@ -15,6 +27,12 @@ import exemple.udemy.java.olx.databinding.ActivityMyAdvertsBinding;
 public class CreateAdvertActivity extends AppCompatActivity {
 
     private ActivityCreateAdvertBinding binding;
+
+    EditText createAdvertTitle;
+    EditText createAdvertDescription;
+    CurrencyEditText createAdvertPrice;
+    MaskEditText createAdvertPhoneNumber;
+    Button createAdvert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +54,45 @@ public class CreateAdvertActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar());
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        components();
+        saveAdvert();
+
         /*
         Window window = this.getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.setStatusBarColor(getColor(R.color.hot_pink_200));
         */
+        createAdvert.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                saveAdvert();
+            }
+        });
+
+
+    }
+
+    private void saveAdvert() {
+        //String value = createAdvertPrice.getHintString();
+        //long value = createAdvertPrice.getRawValue();
+        String value = createAdvertPrice.getText().toString();
+        String price = createAdvertPhoneNumber.getText().toString();
+        Log.d(TAG, "saveAdvert: " + value + " " + price);
+
+    }
+
+
+    private void components() {
+        createAdvertPrice  = binding.currencyEditTextCreateAdvertPrice;
+        createAdvertPhoneNumber = binding.maskEditTextCreateAdvertPhone;
+        createAdvertTitle = binding.editTextCreateAdvertTitle;
+        createAdvertDescription = binding.editTextCreateAdvertDescription;
+        createAdvert = binding.buttonCreateAdvert;
+
+        Locale locale = new Locale("US", "en");
+        createAdvertPrice.setTextLocale(locale);
 
     }
 }
